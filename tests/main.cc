f045485f9ca2d6c62b777963099196406dd783ba
Vadim Sytnikov <vadim.sytnikov@cyberhull.com>
2021-01-24 16:05:29 +0300

    Added project files and library module stubs

---
+++ tests/main.cc
@@ -0,0 +1,27 @@
+/**
+ * C++ Port of the SLALIB library.
+ * Written by Vadim Sytnikov.
+ * Copyright (C) 2021 CyberHULL, Ltd.
+ * All rights reserved.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * -----------------------------------------------------------------------------
+ *
+ * Program entry point.
+ *
+ */
+#include "sla_test.h"
+
+int main(int argc, char** argv) {
+  // TBD
+  return 0;
+}
