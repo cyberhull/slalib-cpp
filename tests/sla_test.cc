6fc970f6dc299cdf646b6d7c1c29adc2fe821f20
Vadim Sytnikov <vadim.sytnikov@cyberhull.com>
2021-02-23 23:19:37 +0300

    Fixed auxiliary test functions

--- tests/sla_test.cc
+++ tests/sla_test.cc
@@ -30,43 +30,43 @@ namespace sla {
 ///////////////////////////////////////////////////////////////////////////////
 
 // reports error in a function test
-static void err(const char *func, const char *test, bool &status) {
-    std::printf("Test '%s' of the function %s() FAILED", test, func);
+static void err(const char* func, const char* test, bool &status) {
+    std::printf("Test '%s' of the function %s() FAILED\n", test, func);
     status = false;
 }
 
 /// Validates a character string result.
-static void vcs(const char *str, const char *str_ok, const char *func, const char *test, bool &status) {
+static void vcs(const char* str, const char* str_ok, const char* func, const char* test, bool &status) {
     if (std::strcmp(str, str_ok) != 0) {
         err(func, test, status);
-        std::printf("\tExpected: '%s'", str_ok);
-        std::printf("\t  Actual: '%s'", str);
+        std::printf("\tExpected: '%s'\n", str_ok);
+        std::printf("\t  Actual: '%s'\n", str);
     }
 }
 
 /// Validates an integer result.
-static void viv(const int val, const int val_ok, const char *func, const char *test, bool &status) {
+static void viv(const int val, const int val_ok, const char* func, const char* test, bool &status) {
     if (val != val_ok) {
         err(func, test, status);
-        std::printf("\tExpected: %d", val_ok);
-        std::printf("\t  Actual: %d", val);
+        std::printf("\tExpected: %d\n", val_ok);
+        std::printf("\t  Actual: %d\n", val);
     }
 }
 
 /// Validates a long result; in FORTRAN implementation of SLALIB, "long" values are INTEGER*4, so C/C++ `int`s
 /// work just fine, there is no need to use `long`s.
-static void vlv(const int val, const int val_ok, const char *func, const char *test, bool &status) {
+static void vlv(const int val, const int val_ok, const char* func, const char* test, bool &status) {
     static_assert(sizeof val >= 4, "`int` values must be at least 32-bit");
     viv(val, val_ok, func, test, status);
 }
 
 /// Validates a double precision floating point result.
 static void vvd(const double val, const double val_ok, const double tolerance,
-    const char *func, const char *test, bool &status) {
+    const char* func, const char* test, bool &status) {
     if (std::fabs(val - val_ok) > tolerance)  {
         err(func, test, status);
-        std::printf("\tExpected: %30.19f", val_ok);
-        std::printf("\t  Actual: %30.19f", val);
+        std::printf("\tExpected: %30.19f\n", val_ok);
+        std::printf("\t  Actual: %30.19f\n", val);
     }
 }
 
@@ -75,8 +75,8 @@ static void vvd(const double val, const double val_ok, const double tolerance,
 ///////////////////////////////////////////////////////////////////////////////
 
 /// Tests sla::airmas() function.
-void t_airmas(bool status) {
-    vvd(airmas(1.2354), 3.015698990074724, 1e-12, "sla::airmas", "", status );
+void t_airmas(bool& status) {
+    vvd(airmas(1.2354), 3.015698990074724, 1e-12, "sla::airmas", "", status);
 }
 
 ///////////////////////////////////////////////////////////////////////////////
