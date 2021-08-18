cb91c41b534a632a09610f32786f42e1c539a9ad
Vadim Sytnikov <vadim.sytnikov@cyberhull.com>
2021-08-18 15:03:55 +0300

    Improved substitutes for F77 math functions (made them template)

--- src/f77_utils.h
+++ src/f77_utils.h
@@ -19,18 +19,25 @@
 #define SLALIB_F77_UTILS_H_INCLUDED
 
 #include <cmath>
+#include <type_traits>
 
 namespace sla {
 
 // C++ counterparts of the F77 math functions; to be inlined at a later stage
-template <typename T>
+template <typename T, std::enable_if_t<std::is_floating_point<T>::value, bool> = true>
 inline T f_sign(T a, T b) {
     const T aa = std::abs(a);
     return b >= 0.0? aa: -aa;
 }
-inline int f_nint(double n) { return (int) std::round(n); }
-inline double f_aint(double n) { return std::trunc(n); }
-inline double f_anint(double n) { return std::round(n); }
+
+template <typename T, std::enable_if_t<std::is_floating_point<T>::value, bool> = true>
+inline int f_nint(T n) { return (int) std::round(n); }
+
+template <typename T, std::enable_if_t<std::is_floating_point<T>::value, bool> = true>
+inline double f_aint(T n) { return std::trunc(n); }
+
+template <typename T, std::enable_if_t<std::is_floating_point<T>::value, bool> = true>
+inline double f_anint(T n) { return std::round(n); }
 
 } // sla
 
