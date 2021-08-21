b434be12aeed2a4b40d482f50f3af4d7478dbd59
Vadim Sytnikov <vadim.sytnikov@cyberhull.com>
2021-08-21 15:22:08 +0300

    Ported sla_RVEROT() function

---
+++ src/rverot.cc
@@ -0,0 +1,44 @@
+/*
+ * C++ Port of the SLALIB library.
+ * Written by Vadim Sytnikov.
+ * Copyright (C) 2021 CyberHULL, Ltd.
+ * All rights reserved.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ */
+#include "slalib.h"
+#include <cmath>
+
+namespace sla {
+
+/**
+ * Calculates velocity component in a given direction due to Earth rotation (single precision).
+ *
+ * The simple algorithm used assumes a spherical Earth, of a radius chosen to give results accurate to about 0.0005
+ * km/s for observing stations at typical latitudes and heights. For applications requiring greater precision, use the
+ * function sla::pvobs().
+ *
+ * Original FORTRAN code by P.T. Wallace / Rutherford Appleton Laboratory.
+ *
+ * @param phi Latitude of observing station (geodetic; radians).
+ * @param pos Apparent RA,Dec (radians).
+ * @param st Local apparent sidereal time (radians).
+ * @return Component of Earth rotation in direction RA,DA (km/s); the result is +ve when the observatory is receding
+ *   from the given point on the sky.
+ */
+float rverot(float phi, const SphericalDir<float>& pos, float st) {
+    // nominal mean sidereal speed of Earth equator in km/s (the actual value is about 0.4651).
+    constexpr float EARTH_SPEED = 0.4655f;
+    return EARTH_SPEED * std::cos(phi) * std::sin(st - pos.sd_a) * std::cos(pos.sd_b);
+}
+
+}
--- src/slalib.h
+++ src/slalib.h
@@ -203,6 +203,7 @@ float sep(const SphericalDir<float>& a, const SphericalDir<float>& b);
 void prebn(double be0, double be1, matrix<double> mat);
 void preces(Catalogue system, double ep0, double ep1, SphericalDir<double>& pos);
 void supgal(const SphericalDir<double>& sgal, SphericalDir<double>& gal);
+float rverot(float phi, const SphericalDir<float>& pos, float st);
 void wait(float delay);
 
 } // sla
